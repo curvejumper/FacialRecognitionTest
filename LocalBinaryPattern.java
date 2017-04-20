bc2c3f77e6c36cd8627645413dbd79a158746736
michael <ortiz.d.michael@gmail.com>
2017-04-20 00:13:48 -0700

    Fixed some scanning loops

--- LocalBinaryPattern.java
+++ LocalBinaryPattern.java
@@ -15,27 +15,49 @@ import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
  */
 public class LocalBinaryPattern {
 
-    private int spacialSize;
+    private final int patternSize = 9;
+    private final int patternMultiple = 3;
 
-    public LocalBinaryPattern(int spacialSize) {
-        this.spacialSize = spacialSize;
+    public LocalBinaryPattern() {
+        
     }
 
     public int[] LocalBinaryPattern(Mat img) {
         Mat imgGrey = img.clone();
         imgGrey.convertTo(img, CV_LOAD_IMAGE_GRAYSCALE);
+        int[] binaryPattern = new int[patternSize];
+        int pixelByte = 0x0000;
         //x, y start at one so no edge detection is needed
         ArrayList<Integer> lbp = new ArrayList<>();
-        for (int i = 0; i < spacialSize; i++) {
-            for (int x = 1 + (i * spacialSize); x < imgGrey.width() - 1; x++) {
-                for (int y = 1 + (i * spacialSize); y < imgGrey.height() - 1; y++) {
-                        if(imgGrey.get(x, y)[0] < imgGrey.get(x + (spacialSize / 2), y + (spacialSize / 2))[0]){
-                            
+        for (int i = 0; i <= patternSize; i++) {
+            int patternIndex = 0;
+            for (int y = 0 + (i * patternMultiple); y < imgGrey.width(); y++) {
+                for (int x = 0 + (i * patternMultiple); x < imgGrey.height(); x++) {
+                    int xCenter = (i *patternMultiple) + 1;
+                    int yCenter = (i * patternMultiple) + 1;
+                    //check if pixel value is larger than center
+                    //AND the pixel being checked isn't the center one
+                    if (patternIndex != (patternSize / 2)) {
+                        //give lower bit a 1 if value is larger than center
+                        if (imgGrey.get(x, y)[0] >= imgGrey.get(i * (patternSize / 2), i * (patternSize / 2))[0]) {
+                            pixelByte = 1 | (pixelByte << 1);
+                        } else {
+                            pixelByte = (pixelByte << 1);
                         }
+                    }
+                    patternIndex++;
+                    
+                    //TODO doublecheck if break just breaks out of current loop. 
+                    if (x % (patternSize  - 1) == 0) {
+                        break;
+                    }
+                }
+                if (y % (patternSize - 1) == 0) {
+                    break;
                 }
             }
         }
-        
+
         return null;
     }
 }
