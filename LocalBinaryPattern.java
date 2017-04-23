c952e1fb1dd0931dc588020fc5fc064bb60a067c
michael <ortiz.d.michael@gmail.com>
2017-04-22 18:15:02 -0700

    Fixed for loops for blocks

--- LocalBinaryPattern.java
+++ LocalBinaryPattern.java
@@ -15,31 +15,30 @@ import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
  */
 public class LocalBinaryPattern {
 
-    private final int patternSize = 9;
-    private final int patternMultiple = 3;
+    private final int numberOfBlocks = 9;
+    private final int blockSize = 3;
 
-    public LocalBinaryPattern() {
-        
+    public LocalBinaryPattern() {   
     }
 
     public int[] LocalBinaryPattern(Mat img) {
         Mat imgGrey = img.clone();
         imgGrey.convertTo(img, CV_LOAD_IMAGE_GRAYSCALE);
-        int[] binaryPattern = new int[patternSize];
+        int[] binaryPattern = new int[numberOfBlocks];
         int pixelByte = 0x0000;
         //x, y start at one so no edge detection is needed
         ArrayList<Integer> lbp = new ArrayList<>();
-        for (int i = 0; i <= patternSize; i++) {
+        for (int i = 0; i <= numberOfBlocks; i++) {
             int patternIndex = 0;
-            for (int y = 0 + (i * patternMultiple); y < imgGrey.width(); y++) {
-                for (int x = 0 + (i * patternMultiple); x < imgGrey.height(); x++) {
-                    int xCenter = (i *patternMultiple) + 1;
-                    int yCenter = (i * patternMultiple) + 1;
+            for (int y = 0 + (i * blockSize); y < imgGrey.width() && y < blockSize * (i + 1); y++) {
+                for (int x = 0 + (i * blockSize); x < imgGrey.height() && x < blockSize * (i + 1); x++) {
+                    int xCenter = ((x + 1) * blockSize) / 2;
+                    int yCenter = ((y + 1) * blockSize) / 2;
                     //check if pixel value is larger than center
                     //AND the pixel being checked isn't the center one
-                    if (patternIndex != (patternSize / 2)) {
+                    if (patternIndex != (numberOfBlocks / 2)) {
                         //give lower bit a 1 if value is larger than center
-                        if (imgGrey.get(x, y)[0] >= imgGrey.get(i * (patternSize / 2), i * (patternSize / 2))[0]) {
+                        if (imgGrey.get(x, y)[0] >= imgGrey.get(i * (numberOfBlocks / 2), i * (numberOfBlocks / 2))[0]) {
                             pixelByte = 1 | (pixelByte << 1);
                         } else {
                             pixelByte = (pixelByte << 1);
@@ -48,11 +47,11 @@ public class LocalBinaryPattern {
                     patternIndex++;
                     
                     //TODO doublecheck if break just breaks out of current loop. 
-                    if (x % (patternSize  - 1) == 0) {
+                    if (x % (numberOfBlocks  - 1) == 0) {
                         break;
                     }
                 }
-                if (y % (patternSize - 1) == 0) {
+                if (y % (numberOfBlocks - 1) == 0) {
                     break;
                 }
             }
